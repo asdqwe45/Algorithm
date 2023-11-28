6ae2400b8d773fb79e37a27bc15cb224bbf23fe1
JaeMyeong0812 <118823358+asdqwe45@users.noreply.github.com>
2023-11-28 11:05:39 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 39.21 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,72 +1,72 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
+    
+    int N,M;
     int[][] maps;
-    int depth;
     int[][] visited;
-    Map<Integer, Integer> lump;
-    int N, M;
-
-    public void bfs(int row, int col) {
-        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        maps[row][col] = depth;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{row, col});
-        int count = 0;
+    int depth;
+    Map<Integer,Integer> lump;
+    
+    public void bfs(int row, int col){
         visited[row][col] = 1;
-        while (!q.isEmpty()) {
+        Queue<int[]> q = new LinkedList<>();
+        maps[row][col] = depth;
+        q.offer(new int[]{row,col});
+        int count = 0;
+        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+        
+        while(!q.isEmpty()){
             int[] front = q.poll();
-            int y = front[0];
-            int x = front[1];
             count++;
-            for (int i = 0; i < 4; i++) {
-                int ny = y + dir[i][0];
-                int nx = x + dir[i][1];
-                if (ny < 0 || nx < 0 || ny >= N || nx >= M)
+            for(int i=0;i<4;i++){
+                int ny = front[0] + dir[i][0];
+                int nx = front[1] + dir[i][1];
+                
+                if(ny<0 || nx<0 || ny>= N || nx>= M)
                     continue;
-                if (maps[ny][nx] == 0)
+                if(maps[ny][nx] ==0)
                     continue;
-                if (visited[ny][nx] == 1)
+                if(visited[ny][nx] == 1)
                     continue;
-                maps[ny][nx] = depth;
-                visited[ny][nx] = 1;
-
-                q.offer(new int[]{ny, nx});
+                visited[ny][nx]=1;
+                maps[ny][nx]=depth;
+                q.offer(new int[]{ny,nx});
+                
             }
         }
-        lump.put(depth++, count);
+        lump.put(depth++,count);
     }
-
+    
     public int solution(int[][] land) {
         int answer = 0;
-        depth = 1;
-        maps = land;
         N = land.length;
         M = land[0].length;
+        maps = land;        
+        depth = 1;
         visited = new int[N][M];
         lump = new HashMap<>();
-
-        // BFS를 돌면서 maps와 cnt 저장
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (visited[i][j] == 1 || maps[i][j] == 0)
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(maps[i][j] == 0 || visited[i][j] == 1)
                     continue;
-                bfs(i, j);
+                bfs(i,j);
             }
         }
-
-        for (int i = 0; i < M; i++) {
+    
+        for(int i=0;i<M;i++){
             int temp = 0;
             Set<Integer> s = new HashSet<>();
-            for (int j = 0; j < N; j++) {
+            for(int j=0;j<N;j++){
                 s.add(maps[j][i]);
             }
-            for (int it : s) {
-                temp += lump.getOrDefault(it, 0);
+            for(int it : s){
+                temp += lump.getOrDefault(it,0);
             }
-            answer = Math.max(answer, temp);
+            answer = Math.max(answer,temp);
         }
-
+        
         return answer;
     }
 }
