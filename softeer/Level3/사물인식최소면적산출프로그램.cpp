297f989b452cb094690b395db8bd6725276f65ab
이재명 <iamwoaud1004@naver.com>
2023-12-18 00:59:01 +0900

    231218 사물인식최소면적산출프로그램.cpp

--- softeer/Level3/사물인식최소면적산출프로그램.cpp
+++ softeer/Level3/사물인식최소면적산출프로그램.cpp
@@ -11,35 +11,22 @@ vector<vector<Node>> v(21);
 int selectNode[21];
 int mini;
 
-int calc() {
-    Node minNode = { 200000000,200000000 };
-    Node maxNode = { -200000000,-200000000 };
-    for (int i = 1; i <= K; i++) {
-        if (selectNode[i] == -1)
-            continue;
-        Node now = v[i][selectNode[i]];
-        minNode.y = min(minNode.y, now.y);
-        minNode.x = min(minNode.x, now.x);
-        maxNode.y = max(maxNode.y, now.y);
-        maxNode.x = max(maxNode.x, now.x);
-    }
-    int result = (maxNode.y - minNode.y) * (maxNode.x - minNode.x);
-
-    return result;
-}
-
-void dfs(int now) {
+void dfs(int now, Node minNode, Node maxNode) {
     if (now > K) {
-        int temp = calc();
+        int temp = (maxNode.y - minNode.y) * (maxNode.x - minNode.x);
         mini = min(temp, mini);
         return;
     }
 
     int size = v[now].size();
     for (int i = 0; i < size; i++) {
-        if (mini > calc()) {
+        Node nowNode = v[now][i];
+        Node minTemp = { min(minNode.y,nowNode.y),min(minNode.x,nowNode.x) };
+        Node maxTemp = { max(maxNode.y,nowNode.y),max(maxNode.x,nowNode.x) };
+        int temp = (maxTemp.y - minTemp.y) * (maxTemp.x - minTemp.x);
+        if (mini > temp || now == 1) {
             selectNode[now] = i;
-            dfs(now + 1);
+            dfs(now + 1, minTemp, maxTemp);
             selectNode[now] = -1;
         }
     }
@@ -48,6 +35,7 @@ void dfs(int now) {
 
 int main(int argc, char** argv)
 {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N >> K;
     for (int i = 0; i < N; i++) {
         int y, x, color;
@@ -55,7 +43,7 @@ int main(int argc, char** argv)
         v[color].push_back({ y,x });
     }
     mini = 2e8;
-    dfs(1);
+    dfs(1, { 20000,20000 }, { -20000,-20000 });
     cout << mini << endl;
     return 0;
 }
